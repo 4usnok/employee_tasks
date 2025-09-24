2213631b08bd6cc5eab240cc3df3b368217e9b38
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-09-24 03:57:15 +0300

    spec-endpoint part 2 (a)

--- task_table/models.py
+++ task_table/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from config import settings
+from employee_table.models import EmployeeTable
 
 
 class TaskTable(models.Model):
@@ -13,21 +14,31 @@ class TaskTable(models.Model):
         max_length=70,
         help_text="название таблицы"
     )
-    link_to_parent_task = models.URLField(
-        max_length=200,
+    link_to_parent_task = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="child_tasks",
         help_text="ссылка на родительскую задачу"
     )
-    performer = models.CharField(
-        max_length=70,
-        help_text="испольнитель"
+    performer = models.ForeignKey(
+        EmployeeTable,
+        on_delete=models.CASCADE,
+        help_text="исполнитель"
     )
     term = models.IntegerField(
-        max_length=5,
+        null=True,
+        blank=True,
         help_text="срок"
     )
     status = models.BooleanField(
         default=True,
         help_text="статус"
+    )
+    work = models.BooleanField(
+        default=True,
+        help_text="взять в работу задачу"
     )
 
     def __str__(self):
--- task_table/views.py
+++ task_table/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import generics
 
 from task_table.models import TaskTable
@@ -32,3 +33,17 @@ class TaskTableRetrieve(generics.RetrieveAPIView):
     """Просмотр одной таблицы задач"""
     queryset = TaskTable.objects.all()
     serializer_class = TaskTableSerializer
+
+class TaskListNotWork(generics.ListAPIView):
+    """
+    Запрашивает из БД задачи, которые не взяты в работу,
+    но от которых зависят другие задачи, взятые в работу
+    """
+    serializer_class = TaskTableSerializer
+
+    def get_queryset(self):
+        tasks_in_work = TaskTable.objects.filter(
+            status=False,
+            child_tasks__status=True
+        )
+        return tasks_in_work.distinct()
