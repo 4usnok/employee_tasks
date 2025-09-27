e04b9c6fd57e860f914d1ea6fd7e0d704597470a
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-09-28 00:38:31 +0300

    spec endpoint

--- task_table/serializers.py
+++ task_table/serializers.py
@@ -5,17 +5,26 @@ from task_table.models import TaskTable
 
 class TaskTableSerializer(serializers.ModelSerializer):
     """Сериализатор для приложения `task_table`"""
+
     class Meta:
         model = TaskTable
-        fields = '__all__'  # Все поля модели
+        fields = "__all__"  # Все поля модели
+
 
 class TaskWithEmployeeSerializer(serializers.ModelSerializer):
-    """Сериализатор для вывода списка объектов в формате: {Важная задача, Срок, [ФИО сотрудника]}"""
-    employee_full_name = serializers.CharField(source='performer.full_name', read_only=True)
+    """Специальный сериализатор для вывода списка объектов в формате: {Важная задача, Срок, [ФИО сотрудника]}"""
+
+    employee_full_name = serializers.CharField(
+        source="performer.full_name", read_only=True
+    )
 
     class Meta:
         # "Важная задача" - это поле name
         # "Срок" - это поле term
         # "ФИО сотрудника" - берем из связанной модели через performer
         model = TaskTable
-        fields = ['name', 'term', 'employee_full_name']  # Соответствует формату из задания
+        fields = [
+            "name",
+            "term",
+            "employee_full_name",
+        ]  # Соответствует формату из задания
