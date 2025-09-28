24653b0dfd86c6e7fb5cd342025940c6ae082caf
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-09-29 00:16:07 +0300

    fixing the project with the mypy linter

--- employee_table/models.py
+++ employee_table/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class EmployeeTable(models.Model):
     """Модель для таблицы сотрудников"""
 
-    full_name = models.CharField(max_length=70, help_text="ФИО")
-    job_title = models.CharField(max_length=70, help_text="Должность")
+    full_name: models.CharField = models.CharField(max_length=70, help_text="ФИО")
+    job_title: models.CharField = models.CharField(max_length=70, help_text="Должность")
 
     def __str__(self):
         return self.full_name
--- task_table/models.py
+++ task_table/models.py
@@ -6,8 +6,8 @@ from employee_table.models import EmployeeTable
 class TaskTable(models.Model):
     """Модель для таблицы задач"""
 
-    name = models.CharField(max_length=70, help_text="название таблицы")
-    parent_task = models.ForeignKey(
+    name: models.CharField = models.CharField(max_length=70, help_text="название таблицы")
+    parent_task: models.ForeignKey = models.ForeignKey(
         "self",
         on_delete=models.CASCADE,
         null=True,
@@ -15,15 +15,15 @@ class TaskTable(models.Model):
         related_name="child_tasks",
         help_text="родительская задача",
     )
-    performer = models.ForeignKey(
+    performer: models.ForeignKey = models.ForeignKey(
         EmployeeTable,
         on_delete=models.CASCADE,
         help_text="исполнитель",
         related_name="task_table",
     )
-    term = models.IntegerField(null=True, blank=True, help_text="срок")
-    status = models.BooleanField(default=True, help_text="статус")
-    work = models.BooleanField(default=True, help_text="взять в работу задачу")
+    term: models.IntegerField = models.IntegerField(null=True, blank=True, help_text="срок")
+    status: models.BooleanField = models.BooleanField(default=True, help_text="статус")
+    work: models.BooleanField = models.BooleanField(default=True, help_text="взять в работу задачу")
 
     def __str__(self):
         return self.name
